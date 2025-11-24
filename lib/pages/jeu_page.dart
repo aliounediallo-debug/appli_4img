2b4bbfd8c96b4b0ecb977a6f20314ccb15a80fca
alioune <aliounebadarandiaye.diallo@unchk.edu.sn>
2025-11-24 16:23:18 +0300

    niveau 1 et niveau 2

---
+++ lib/pages/jeu_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JeuPage extends StatefulWidget {
+  const JeuPage({super.key});
+
+  @override
+  State<JeuPage> createState() => _JeuPageState();
+}
+
+class _JeuPageState extends State<JeuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
